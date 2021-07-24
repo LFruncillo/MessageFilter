ec5335807953803ad0d3a49e09d252882381d824
LFruncillo <luke.fruncillo@outlook.com>
2021-07-25 00:10:56 +0100

    Added comments

--- messagefilter.py
+++ messagefilter.py
@@ -1,14 +1,20 @@
+# Blacklisted words.
 words = ['Pog', 'Chad']
+
+# Set user login details.
 regusername = 'WideLuke'
 regpassword = '12345'
 
+# Login form.
 print('Login')
 logusername = input('Username:')
 logpassword = input('Password:')
 
+# Checks if user details are correct.
 if logusername == regusername and logpassword == regpassword:
     choice = input(logusername + ':')
 
+# Allows the user to send a message. If message contains a blacklisted word then the message gets stored in a txt file, else the message is ignored.
 if choice == 'Send message':
     message = input(logusername + ': ')
     for word in words:
@@ -18,6 +24,7 @@ if choice == 'Send message':
             f.write(logusername + ': ' + message + ', ')
             f.close
 
+# Prints words from messages that are blacklisted. 
 if choice == 'Check messages':
     with open(r"C:\Users\LukeF\Documents\GitHub\MessageFilter\message.txt", "r") as file:
         contents = file.read()
